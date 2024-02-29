c0c87e62573cd04a029d755f4229fa7571a8fe70
vladimir_lukyanenko <vladimir.lukyanenko@kitrum.com>
2024-02-29 12:06:53 +0200

    Initial commit

---
+++ src/AppSettings/ApiSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace AppSettings
+{
+    public class ApiSettings
+    {
+        public string ApiKey { get; set; } = string.Empty;
+        public string ApiSecret { get; set; } = string.Empty;
+        public string BearerToken { get; set; } = string.Empty;
+    }
+}
---
+++ src/AppSettings/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace AppSettings
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false);
+
+            IConfiguration config = builder.Build();
+
+            var apiSettings = config.GetSection("ApiSettings").Get<ApiSettings>()!;
+
+            Console.WriteLine(apiSettings.ApiKey);
+            Console.WriteLine(apiSettings.ApiSecret);
+            Console.WriteLine(apiSettings.BearerToken);
+        }
+    }
+}
